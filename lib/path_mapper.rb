3b0230ddf80a3a3566961ba71da2da0b3ef90db3
Алексей Игрычев <alexey.igrychev@flant.ru>
2016-02-24 20:15:34 +0300

    added: * name; * _grep, FilesIterator;

--- lib/path_mapper.rb
+++ lib/path_mapper.rb
@@ -3,9 +3,11 @@ require 'path_mapper/version'
 module PathMapper
   class Mapper
     attr_reader :path
+    attr_reader :_name
 
     def initialize(path)
       @path = path
+      @_name = path.scan(/[^\/]+/).last
     end
 
     def f(m)
@@ -16,8 +18,13 @@ module PathMapper
           File.read(path)
         end
       else
-        NullObject.new
+        NullObject.new(m.to_s)
       end
+    end
+
+    def _grep(reg)
+      files = Dir["#{@path}/**/*"].select {|f| f =~ reg }
+      FilesIterator.new(files)
     end
 
     def method_missing(m, *args, &block)
@@ -26,20 +33,47 @@ module PathMapper
   end
 
   class NullObject < BasicObject
+    attr_reader :_name
+
+    def initialize(name)
+      @_name = name
+    end
+
     def method_missing(m, *args, &block)
       if nil.respond_to? m
         nil.send m, *args, &block
       else
+        @_name = m.to_s
         self
       end
     end
 
-    def empty()
+    def empty
       true
     end
 
-    def any?()
+    def any?
       false
     end
   end
+
+  class FilesIterator
+    include Enumerable
+
+    def initialize(files)
+      @files = files
+    end
+
+    def each
+      @files.each do |f|
+        obj = if File.directory? f
+          Mapper.new(f)
+        else
+          File.read(f)
+        end
+        yield obj
+      end
+    end
+  end
 end
+
