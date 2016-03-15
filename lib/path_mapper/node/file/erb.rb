88e35f23d95c365e9b3f8a877408bb7d4023138b
Alexey Igrychev <pes.sutuliy@gmail.com>
2016-03-15 17:05:21 +0300

    +

--- lib/path_mapper/node/file/erb.rb
+++ lib/path_mapper/node/file/erb.rb
@@ -2,11 +2,8 @@ module PathMapper
   module Node
     module File
       module Erb
-        @@context_options = {}
-
         def erb(**kwargs)
-          options = @@context_options.merge(kwargs)
-          ERB.new(self.value).result(OpenStruct.new(options).instance_eval { binding })
+          ERB.new(self.value).result(OpenStruct.new(kwargs).instance_eval { binding })
         rescue Exception => e
           raise NetStatus::Exception, e.net_status.merge!( code: :erb_error, data: { error: "#{e.backtrace.first.sub! '(erb)', self.path.to_s}: #{e.message}" })
         end
